2c92869b752bb8e0fe74c3183f40f3f58b7b906d
Timmy Willison <timmywillisn@gmail.com>
2015-07-13 15:26:26 -0400

    Release 3.0.0-alpha1

---
+++ src/css/hiddenVisibleSelectors.js
@@ -0,0 +1,13 @@
+define([
+	"../core",
+	"../selector"
+], function( jQuery ) {
+
+jQuery.expr.filters.hidden = function( elem ) {
+	return !jQuery.expr.filters.visible( elem );
+};
+jQuery.expr.filters.visible = function( elem ) {
+	return !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length );
+};
+
+});
